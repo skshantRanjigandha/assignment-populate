5c8b59a4aff11e2ff8019c11657b5608cc02de30
Sushant_KB-uranium <102942750+skshantRanjigandha@users.noreply.github.com>
2022-04-12 21:06:31 +0530

    first commit

--- src/controllers/randomController.js
+++ src/controllers/randomController.js
@@ -1,7 +1,9 @@
 let addToArray= function (req, res) {
     let x= req.body.number
     console.log(x)
-    let arr= [2, 5, 11, 14]
+
+    
+    let arr= [2, 5, 11, 14,]
     arr.push(x)
     res.send( {  msg: "post req 3", data: arr  } )
 }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
-const express = require('express');
+const express = require('express')
+const app = express()
+
 var bodyParser = require('body-parser');
 
-const route = require('./routes/route.js');
+const route = require('./routes/route');
+const res = require('express/lib/response');
 
-const app = express();
+const  mongoose= require('mongoose')
 
-app.use(bodyParser.json());
+
+mongoose.connect("mongodb+srv://sushantkb:TM8BFATdO1usEQ5Q@cluster0.ubaqb.mongodb.net/bookschema?retryWrites=true&w=majority",{useNewUrlParser: true})
+.then(()=>
+console.log("mongoDb is connected"))
+.catch( err =>  console.log(err))
+
+
+app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/', route);
-
+    
 app.listen(process.env.PORT || 3000, function() {
-    console.log('Express app running on port ' + (process.env.PORT || 3000))
+
+    
+console.log('Express app running on port ' + (process.env.PORT || 3000))
 });
